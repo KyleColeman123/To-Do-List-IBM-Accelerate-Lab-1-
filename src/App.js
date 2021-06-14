096d37741f995f3c713cb47394686a2012599aca
atang816 <alec.tang@sjsu.edu>
2021-06-14 09:47:23 -0700

    Added input and display task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import data from "./data.json";
+
 import Header from "./component/header"
+import ToDoList from "./component/ToDoList";
+import ToDoForm from './component/ToDoForm';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput}];
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
-    <Header/>
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} />
+      <ToDoForm addTask={addTask}/>
     </div>
   );
 }
